09e0f5a9540b8f5415d2e8c4169fc5265a5e0fe4
Kunalpal215 <kunalpal215@gmail.com>
2022-10-29 12:49:08 +0530

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/",(req,res) => {
-    res.send("Hello World");
+    res.send("App is working");
 });
 app.listen('3000',() => {
     console.log("Server Started");
