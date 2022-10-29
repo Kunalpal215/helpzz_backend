c99531c0e91c64c000f41a938e30f22ccdb94333
Kunalpal215 <kunalpal215@gmail.com>
2022-10-29 13:34:54 +0530

    added export for auth router

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -3,3 +3,4 @@ const bcryptjs = require("bcryptjs");
 const authController = require("../controllers/authController");
 authRouter.post("/signup", authController.signupController);
 authRouter.post("/login", authController.loginController);
+module.exports = {authRouter};
