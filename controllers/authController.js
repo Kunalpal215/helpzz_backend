64e3acfc15ee75f721104bd5651ed64f713f1e1b
Kunalpal215 <kunalpal215@gmail.com>
2022-10-30 13:57:37 +0530

    fixed authcontroller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,36 +1,45 @@
 const user = require("../models/userModel");
+const bcryptjs=require("bcryptjs");
 exports.signupController = async (req,res) => {
-    let useremail = req.body.useremail;
-    let userpassword = req.body.userpassword;
-    let username = req.body.username;
-    const check = await user.findOne({$or : [{"useremail" : useremail},{"username" : username}]});
-    if(check){
-        res.status(400).json({"result" : "useremail or username is already in use"});
-        return;
-    }
-    const hashedPassword = bcryptjs.hashSync(userpassword,10);
-    userpassword=hashedPassword;
-    const newUser = user({useremail: useremail,userpassword: userpassword, username : username });
-    const savedUser = await newUser.save();
-    if(savedUser){
+    try{
+        let useremail = req.body.useremail;
+        let userpassword = req.body.userpassword;
+        let username = req.body.username;
+        const check = await user.findOne({$or : [{"useremail" : useremail},{"username" : username}]});
+        if(check){
+            res.status(400).json({"result" : "useremail or username is already in use"});
+            return;
+        }
+        const hashedPassword = bcryptjs.hashSync(userpassword,10);
+        userpassword=hashedPassword;
+        const newUser = user({useremail: useremail,userpassword: userpassword, username : username });
+        const savedUser = await newUser.save();
         res.json({"result" : "Signed up Successfully"});
-        return;
     }
-    res.status(400).json({"result" : "An error occured while signing up"});
+    catch(err){
+        console.log(err);
+        res.status(400).json({"result" : "An error occured while signing up"});
+    }
 };
 
 exports.loginController = async (req,res) => {
-    let useremail = req.body.useremail;
-    let userpassword = req.body.userpassword;
-    const dbUser = await user.findOne({useremail : useremail});
-    if(!dbUser){
-        res.status(400).json({"result" : "Useremail or Password maybe incorrect"});
-        return;
+    try{
+        let useremail = req.body.useremail;
+        let userpassword = req.body.userpassword;
+        const dbUser = await user.findOne({useremail : useremail});
+        if(!dbUser){
+            res.status(400).json({"result" : "Useremail or Password maybe incorrect"});
+            return;
+        }
+        let validPassword = bcryptjs.compareSync(userpassword,dbUser.userpassword);
+        if(validPassword==false){
+            res.status(400).json({"result" : "Useremail or Password maybe incorrect"});
+            return;
+        }
+        res.json({"result" : "Loggedin Successfully"});
     }
-    let validPassword = bcryptjs.compareSync(userpassword,dbUser.userpassword);
-    if(validPassword==false){
-        res.status(400).json({"result" : "Useremail or Password maybe incorrect"});
-        return;
+    catch(err){
+        console.log(err);
+        res.status(400).json({"result" : "An error occured while signing up"});
     }
-    res.json({"result" : "Loggedin Successfully"});
 };
